cc879cdf1195a19c8a88b942e2c3cb351f95819a
Swarnadarsini V <102981100+4101swarna@users.noreply.github.com>
2024-11-12 22:17:53 +0530

    commit new folders
    Adding updated files into the git repository

---
+++ frontend/src/components/Brand.jsx
@@ -0,0 +1,11 @@
+const Brand = () => {
+  return (
+    <div className="flex justify-center items-center w-full">
+      <span className="text-white text-4xl font-abril font-semibold">
+        ShopHeris
+      </span>
+    </div>
+  );
+};
+
+export default Brand;
---
+++ frontend/src/components/Loader.jsx
@@ -0,0 +1,7 @@
+function Loader() {
+  return (
+    <div className="animate-spin rounded-full h-18 w-18 border-t-3 border-blue-500 border-opacity-40 "></div>
+  );
+}
+
+export default Loader;
